6a509a40de06b4d6a032f3898724016ae99aaa11
Miguel Ludert <miguelludert@Missy.local>
2022-08-15 07:14:19 -0700

    adding codfe changes while i figure out publishing

--- packages/sample-microservice/src/index.ts
+++ packages/sample-microservice/src/index.ts
@@ -1,7 +1,6 @@
 import { App, Stack } from 'aws-cdk-lib';
 import { AppsyncSchemaTransformer } from '@miguelludert/appsync';
 import { join } from 'path';
-import { Construct } from 'constructs';
 import { paramCase } from 'change-case';
 
 const app = new App();
